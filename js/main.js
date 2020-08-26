a8b6a73077d939f1c94e0b0d26340f0801bc2676
Tawanda Mavondo <tawandagdmavondo@gmail.com>
2020-08-26 23:26:28 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 
 const displayVideoToggles = document.querySelectorAll('.display-video');
 
-let displayVideo = (e) => {
-    e.path[2].classList.add('show-video');
+let displayVideo = (e, it) => {
+    it.childNodes[1].classList.add('show-video')
 };
 
 displayVideoToggles.forEach(it => {
-    it.addEventListener('click', displayVideo);
+    it.addEventListener('click', (e) => displayVideo(e, it));
 });
 
